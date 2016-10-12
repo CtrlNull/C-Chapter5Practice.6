bdae3d01e1afd4c434acce63f38c9c5f5a292770
Raphael Fonseca <raphaelfonseca88@hotmail.com>
2016-10-12 12:55:20 -0700

    Add files via upload

---
+++ ifElsePractiveTwoGradesCont/ifElseifPractiveTwoGradesCont.cpp
@@ -0,0 +1,39 @@
+/*
+	This program uses an if/elseif statement to assign a letter grade
+	(a, b, c, d, or f) to numeric test score.
+*/
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Constants for grade thresholds
+	const int A_Score = 90,
+		B_Score = 80,
+		C_Score = 70,
+		D_Score = 60;
+
+	int testScore; // To hold a numeric test score
+
+	// Get the numeric test score. 
+	cout << "Enter your numeric test score and i will \n"
+		<< "tell you the letter grade you earned: ";
+	cin >> testScore;
+
+	// Determine the letter grade.
+	if (testScore >= A_Score)
+		cout << "Your grade is A. \n";
+	else if (testScore >= B_Score)
+		cout << "Your grade is B. \n";
+	else if (testScore >= C_Score)
+		cout << "Your grade is C. \n";
+	else if (testScore >= D_Score)
+		cout << "Your grade is D. \n";
+	else
+		cout << "Your grade is F. \n";
+
+    return 0;
+}
+
